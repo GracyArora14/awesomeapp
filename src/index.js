526994be666c96aa2196842b133d18f61a126982
Gracy Arora <gr4059@srmist.edu.in>
2022-08-24 19:29:12 +0530

    ok

--- src/index.js
+++ src/index.js
@@ -1,17 +1,34 @@
 import React from "react";
 import ReactDOM  from "react-dom";
+import './index.css';
 
-const fname='Gracy Arora';
-const currDate = new Date().toLocaleDateString();
-const currTime = new Date().toLocaleTimeString();
+const name= "Gracy";
+const img1= "https://picsum.photos/200/300";
+const img2= "https://picsum.photos/250/300";
+const img3= "https://picsum.photos/300/300";
+const links ="https://evarsity.srmist.edu.in/srmsip/";
+const heading={
+    color: "blue",
+    textAlign: "center",
+    textTransform: "capitalize",
+    fontWeight: "bold",
+    margin: '30px',
+    fontFamily: "'Aboreto', cursive",
+  };
 
 
 ReactDOM.render(
     <>
-    <h1>Hello ! My name is {fname}</h1>
-    <p>The Curent Date is {currDate}</p>
-    <p>The Curent Time is {currTime}</p>
+     <h1 style={heading}>Hello ! My name is {name} </h1> 
+
+    <div className="imgs">
+      <img src={img1} alt="Random images" />
+      <img src={img2} alt="Random images" />
+      <a href= {links} target="_grace" >
+       <img src={img3} alt="Random images" />
+      </a>
+     </div>
     </>,
-     document.getElementById('root')
+     document.getElementById("root")
 
 );
